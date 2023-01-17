5f6a40dc027d128da4e2679caf566fa7d2d113c7
ArsenKesoyan <121717571+ArsenKesoyan@users.noreply.github.com>
2023-01-18 01:03:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+function f(argument) {
+	if (true) {
+		lo__in.style.display='none'
+		lu__in.style.display='none'
+		li__in.style.display='none'
+		le__in.style.display='none'
+	}
+}
+function product(argument) {
+	var li__in=document.getElementById('li__in')
+	if (true) {
+		li__in.style.display='unset'
+		lu__in.style.display='none'
+	}
+	else{
+		li__in.style.display='none'
+		lu__in.style.display='none'
+		lo__in.style.display='none'
+		le__in.style.display='none'
+	}
+}
+
+function prod(argument) {
+	var li__in=document.getElementById('li__in')
+	
+	if (true)  {
+		li__in.style.display='none'
+	}
+
+}
+
+
+function future(argument) {
+	var lu__in=document.getElementById('lu__in')
+	if (true) {
+		lu__in.style.display='unset'
+		li__in.style.display='none'
+		le__in.style.display='none'
+		lo__in.style.display='none'
+	}
+	
+}
+function fut(argument) {
+	var lu__in=document.getElementById('lu__in')
+	
+	if (true)  {
+		lu__in.style.display='none'
+
+	}
+
+}
+function tt(argument) {
+	var le__in=document.getElementById('le__in')
+	if (true) {
+		le__in.style.display='unset'
+		li__in.style.display='none'
+		lu__in.style.display='none'
+		lo__in.style.display='none'
+	}
+	
+}
+function t(argument) {
+	var le__in=document.getElementById('le__in')
+	
+	if (true)  {
+		lu__in.style.display='none'
+		li__in.style.display='none'
+		le__in.style.display='none'
+		lo__in.style.display='none'
+
+	}
+
+}
+function uss(argument) {
+	var lo__in=document.getElementById('lo__in')
+	if (true) {
+		lo__in.style.display='unset'
+		le__in.style.display='none'
+		li__in.style.display='none'
+		lu__in.style.display='none'
+	}
+	
+}
+function us(argument) {
+	var lo__in=document.getElementById('lo__in')
+	
+	if (true)  {
+		lo__in.style.display='none'
+		lu__in.style.display='none'
+		li__in.style.display='none'
+		le__in.style.display='none'
+
+	}
+
+}
+function pr(argument) {
+	if (true) {
+		lo__in.style.display='none'
+		lu__in.style.display='none'
+		li__in.style.display='none'
+		le__in.style.display='none'
+	}
+}
